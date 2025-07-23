f14d07cb4a50d04cff60d32e5ac71715d601f288
Tanner Chung <tannerchung@users.noreply.github.com>
2025-07-23 12:16:04 -0400

    Updated vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   resolve: {
     alias: {
       'langsmith': '/src/services/langsmith-mock'
-  },
+      },
+  }
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
